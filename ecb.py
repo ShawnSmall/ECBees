c3a42076ce6f2021ded6c912ba2b0c4bf4803136
ShawnSmall <smsmall@umass.edu>
2020-12-03 02:45:24 -0500

    initial commit

---
+++ ecb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env
+
+# visual data collection
+# make sure start_motion_daemon=yes is in the motion.conf file
+sudo motion
+
+
+
+
+
+
+
+# vibroacoustic data collection
+
+
+
+
+
+# shared network program for saving data on remote device
+
+
+
+
+
+# save data to micro SD card
